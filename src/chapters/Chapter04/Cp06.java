f7338649c1fe5057b8f9196582e9ee92caeab85d
OmerDokur <omerdokur35@gmail.com>
2022-09-02 23:41:10 +0200

    Checkpoints of Chapter 3

--- src/chapters/Chapter04/Cp06.java
+++ src/chapters/Chapter04/Cp06.java
@@ -5,9 +5,19 @@ public class Cp06 {
 	public static void main(String[] args) {
 		//String Class
 		
-		String str = "Hello Java";
+		String str = "Hello Java! ";
 		System.out.println(str.length());
+		
+		System.out.println(str.charAt(0));
+		System.out.println(str.charAt(9));
+		
+		String str2= "Java is good.";
+		String str3= str.concat(str2);
+		
+		System.out.println(str3);
 
+		
+		
 	}
 
 }
