7a9b27b8cda15f98864c9347ec25d850bc73db21
OmerDokur <omerdokur35@gmail.com>
2022-09-03 23:49:33 +0200

    Exercises of Chapter 4

---
+++ src/chapters/Chapter04/Ex04.java
@@ -0,0 +1,19 @@
+package chapters.Chapter04;
+
+import java.util.Scanner;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		  System.out.print("Enter the side:");
+		 
+		  double s = input.nextDouble();
+		  double area = 6 * Math.pow(s, 2) / (4 * Math.tan(Math.PI / 6));
+		 
+		  area = Math.round(area * 100) / 100.0;
+		  System.out.println("The area of the hexagon is " + area);
+
+	}
+
+}
