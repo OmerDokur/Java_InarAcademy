4584f20437b62e69a5ff10a1f0a4a45e329226d7
OmerDokur <omerdokur35@gmail.com>
2022-09-06 23:42:39 +0200

    Checkpoints of Chapter 4

---
+++ src/chapters/Chapter04/Cp14.java
@@ -0,0 +1,24 @@
+package chapters.Chapter04;
+
+public class Cp14 {
+
+	public static void main(String[] args) {
+		// Substring beginIndex, endIndex
+
+				String str = "Merhaba Java";
+				String str2 = str.substring(6);
+				System.out.println(str2);
+				
+				str2 = str.substring(3, 7);
+				System.out.println(str2);
+				
+				str2 = str.replaceAll("haba", "gurkan");
+				System.out.println(str2);
+				
+				String message = "Welcome to Java";
+				message = message.substring(0, 11) + "HTML";
+				System.out.println(message);
+
+	}
+
+}
---
+++ src/chapters/Chapter04/Cp16.java
@@ -0,0 +1,18 @@
+package chapters.Chapter04;
+
+public class Cp16 {
+
+	public static void main(String[] args) {
+		//String methods.
+		
+				String str ="12345";
+				
+				int num = Integer.parseInt(str);
+				
+				System.out.println(num);
+				
+				double doubleValue = Double.parseDouble("  \t55.78\n");
+
+	}
+
+}
