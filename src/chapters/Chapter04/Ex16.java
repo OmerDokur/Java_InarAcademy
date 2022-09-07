90517672dcc128148aa08ae129bb22c97c438bab
OmerDokur <omerdokur35@gmail.com>
2022-09-07 22:53:41 +0200

    Exercises of Chapter 4

---
+++ src/chapters/Chapter04/Ex16.java
@@ -0,0 +1,13 @@
+package chapters.Chapter04;
+
+public class Ex16 {
+
+	public static void main(String[] args) {
+		int n = (int) (65 + Math.random() * 26);
+		  char c = (char) n;
+		   
+		  System.out.println("The random letter is " + c);
+
+	}
+
+}
