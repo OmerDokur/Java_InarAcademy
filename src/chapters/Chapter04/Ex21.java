d6dfdb86fa03123e0ec2ac66c2519a5efc74e84a
OmerDokur <omerdokur35@gmail.com>
2022-09-08 22:56:06 +0200

    Exercises of Chapter 4

---
+++ src/chapters/Chapter04/Ex21.java
@@ -0,0 +1,27 @@
+package chapters.Chapter04;
+
+import java.util.Scanner;
+
+public class Ex21 {
+
+	public static void main(String[] args) {
+		 Scanner input = new Scanner(System.in);
+		 
+		  System.out.print("Enter a SSN:");
+		  String s = input.nextLine();
+		 
+		  for (int i = 0; i < s.length(); i++) {
+		   if ((i == 3 || i == 6) && s.charAt(i) == '-') {
+		    continue;
+		   }
+		   if (!Character.isDigit(s.charAt(i))) {
+		    System.out.println(s + " is an invalid social security number");
+		    System.exit(0);
+		   }
+		  }
+		 
+		  System.out.println(s + " is a valid social security number");
+
+	}
+
+}
