79ca39519c8bb090db0924b0db99eca143c3fc36
OmerDokur <omerdokur35@gmail.com>
2022-09-21 23:35:52 +0200

    Exercises of Chapter 5

---
+++ src/chapters/Chapter05/Ex2302.java
@@ -0,0 +1,16 @@
+package chapters.Chapter05;
+
+public class Ex2302 {
+
+	public static void main(String[] args) {
+		//Demonstrate cancellation errors
+		
+		double checkSum = 0.0;
+        for (int i = 1000; i >= 1; i--) {
+            checkSum += 1.0 / i;
+        }
+        System.out.println("Result of computing sum of the series from right to left was " + checkSum);
+
+	}
+
+}
---
+++ src/chapters/Chapter05/Ex2402.java
@@ -0,0 +1,25 @@
+package chapters.Chapter05;
+
+public class Ex2402 {
+
+	public static void main(String[] args) {
+		//Sum a series
+		
+		 double res = 0.0;
+	        System.out.println("Cancellation error example calculating from right to left: ");
+	        for (int i = 1000; i >= 1; i--) {
+	            res += 1.0 / i;
+	        }
+	        System.out.println("Total Sum: " + res);
+	        double res2 = 0.0;
+	        System.out.println("Cancellation error example calculating from left to right: ");
+	        for (int i = 1; i <= 1000; i++) {
+	            res2 += 1.0 / i;
+	        }
+	        System.out.println("Total Sum: " + res2);
+	        double d = res - res2;
+	        System.out.println("By going from left to right, we lost accuracy of " + d);
+
+	}
+
+}
