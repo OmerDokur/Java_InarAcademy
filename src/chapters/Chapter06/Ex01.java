f487f53b709b2ed48e2b2d3b4538ff81a26dae6a
OmerDokur <omerdokur35@gmail.com>
2022-09-24 23:55:46 +0200

    Exercises of Chapter 6

---
+++ src/chapters/Chapter06/Ex01.java
@@ -0,0 +1,21 @@
+package chapters.Chapter06;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) {
+            System.out.printf("%8d", getPentagonalNumber(i));
+            if (i % 10 == 0) {
+                System.out.println("");
+            }
+
+        }
+
+    }
+
+    public static int getPentagonalNumber(int n) {
+        return n * (3 * n - 1) / 2;
+    }
+}
+
+	
